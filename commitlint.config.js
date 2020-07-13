cc9ad8470e83904455da7d64c13d90f1ecf0c1dd
Aupam Gore <anupamgore@gmail.com>
2020-07-13 16:19:15 +0530

    Storybook typescript (#11)
    * refactor(common): adding ts support for storybook
    
    * refactor(common): adding deps-dev to commitlint scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,14 +2,14 @@
 //TODO - need to add to scope-enum
 
 const Configuration = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-enum': [2, 'always', ['build', 'deps', 'common']],
-        'scope-case': [0],
-        'scope-empty': [2, 'never'],
-        'subject-case': [0],
-        'body-max-line-length': [0]
-    }
-}
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "always", ["build", "deps", "common", "deps-dev"]],
+    "scope-case": [0],
+    "scope-empty": [2, "never"],
+    "subject-case": [0],
+    "body-max-line-length": [0],
+  },
+};
 
-module.exports = Configuration
+module.exports = Configuration;
